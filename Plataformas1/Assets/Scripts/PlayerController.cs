6cd541cabf37307ed287ac246fd29acbd3d59194
Alberto <arodriguezg@iesch.org>
2021-02-24 16:28:42 +0100

    jugadorSalta

--- Plataformas1/Assets/Scripts/PlayerController.cs
+++ Plataformas1/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     // Velocidad a la que se va a mover el jugador
     public float velocidad = 2.5f;
+    public float fuerzaSalto = 2.5f;
+    // Controlo si estoy en el suelo
+    public Transform groundCheck; //desde donde haremos el chequeo del suelo
+    public LayerMask groundLayer;// seleccionaremos qué layer es el suelo
+    public float groundCheckRadius;
     // Referencias
     private Rigidbody2D _rigidbody;
     private Animator _animator;
     //Movimiento
     private Vector2 _movimiento;
+    private bool _facingRight = true;
+    private bool _enSuelo;
+
 
 
     private void Awake()
@@ -32,8 +40,24 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         _movimiento = new Vector2(horizontalInput, 0f);
 
+        // Controlamos hacia donde mira el personaje
+        if ( horizontalInput < 0f && _facingRight == true)
+        {
+            Flip();
+        } else if ( horizontalInput > 0f && _facingRight == false )
+        {
+            Flip();
+        }
+
+        //Está el personaje en el suelo?
+        _enSuelo = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        // Controlamos el Salto
+        if (Input.GetButtonDown("Jump") && _enSuelo == true)
+        {
+            _rigidbody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
     }
-    //Es para mover cualquier objeto enUnity
+    //Es para mover cualquier objeto en Unity
     private void FixedUpdate()
     {
         float horizontalVelocity = _movimiento.normalized.x * velocidad;
@@ -44,5 +68,15 @@ public class PlayerController : MonoBehaviour
     private void LateUpdate()
     {
         _animator.SetBool("Idle", _movimiento == Vector2.zero);
+        _animator.SetBool("enSuelo", _enSuelo);
+        _animator.SetFloat("VerticalVelocity", _rigidbody.velocity.y);
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        float localScaleX = transform.localScale.x;
+        localScaleX = localScaleX * -1f;
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
     }
 }
